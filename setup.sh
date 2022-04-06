11f4e5d6b014d63eb1184d5ff91e154a67da5ea8
Jeremy A. Puhlman <jpuhlman@mvista.com>
2022-04-06 18:59:10 +0000

    Update machine

--- setup.sh
+++ setup.sh
@@ -53,7 +53,7 @@ LAYER@https://github.com/MontaVista-OpenSourceTechnology/meta-montavista-cgl.git
 LAYER@https://github.com/MontaVista-OpenSourceTechnology/meta-arm.git;branch=master;layer=meta-arm-toolchain \
 LAYER@https://github.com/MontaVista-OpenSourceTechnology/meta-arm.git;branch=master;layer=meta-arm \
 LAYER@https://github.com/MontaVista-OpenSourceTechnology/meta-ti.git;branch=master;layer=meta-ti-bsp \
-MACHINE@j721e-evm \
+MACHINE@j721e-evm-k3r5 \
 DISTRO@mvista-cgx \
 "
 TOPDIR=$(dirname $THIS_SCRIPT)
